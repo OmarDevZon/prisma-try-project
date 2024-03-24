53d9cd8f119c7e8990620baf45f86ed43e9365e4
omar-web-dev-2 <apoloweb2.0@gmail.com>
2024-03-24 12:38:41 +0600

    find

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,38 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
+  // Create a new
+  // const result = await prisma.post.create({
+  //   data: {
+  //     title: "this is a test",
+  //     contant: "this is my first post2",
+  //     author: "Omar Faruk",
+  //   },
+  // });
 
-  // Create a new 
-  const result = await prisma.post.create({
-    data: {
-      title: "this is a test",
-      contant: "this is my first post2",
-      author: "Omar Faruk",
+  // find all data
+  const findMany = await prisma.post.findMany();
+  // find fist result from db
+  const findOneByID = await prisma.post.findFirst({
+    where: {
+      id: 5,
     },
   });
+  // find fist result from db and return error message
+  const findOneByIdTrowError = await prisma.post.findFirstOrThrow({
+    where: {
+      id: 15,
+    },
+  });
+  
+  
+  // find fist result from db and return error message
+  const findUnique = await prisma.post.findUniqueOrThrow({
+    where: {
+      id: 15,
+    },
+  });
+  console.log(findUnique);
 };
 
 main();
